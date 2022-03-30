183930d3cefa43666565512b262eeed621e071f4
Ali Mousavi <64815288+mnizhadali-afg@users.noreply.github.com>
2022-03-30 12:18:21 +0500

    import and export practiced

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as c from "./calculator.js";
+import add, { subtract, multiply, divide } from "./calculator.js";
 
 ReactDOM.render(
   <ul>
-    <li>{c.default(2, 2)}</li>
-    <li>{c.multiply(2, 3)}</li>
-    <li>{c.subtract(7, 2)}</li>
-    <li>{c.divide(5, 2)}</li>
+    <li>{add(2, 2)}</li>
+    <li>{subtract(7, 2)}</li>
+    <li>{multiply(2, 3)}</li>
+    <li>{divide(5, 2)}</li>
   </ul>,
   document.getElementById("root")
 );
